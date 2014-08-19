c4198f5eed0d612932570ca9b89f4a562fe485b1
Jason Rushton <github.com@schtuph.com>
2014-08-19 17:09:27 -0600

    require page.js and start it; using a simple progress bar overlay as a loading indicator

--- routes/client.js
+++ routes/client.js
@@ -1,35 +1,34 @@
 'use strict';
 
 var qs = require('querystring');
+var page = require('page');
 
 
 var globalRequire = require('../lib/globalRequire');
 var _ = globalRequire('lodash');
 var $ = globalRequire('jQuery');
-var page = globalRequire('page');
 
 var sharedRoutes = require('./shared');
 var libTemplates = require('../lib/templates');
 var templateRenderer = libTemplates(window.templates);
 
 
-
-
-window.wasPrerendered;
 var transitionTime = 300;
 
 
 module.exports = function(rootNode, prerendered) {
-	// var rootNode = document.getElementById(rootElement);
-
 	window.wasPrerendered = prerendered;
-
-	console.log('routes', rootNode);
 
 	_.forEach(
 		sharedRoutes,
 		attachRoute.bind(null, rootNode)
 	);
+
+
+	$('#loading').hide().removeClass('hidden');
+
+
+ 	page.start({click: true});
 };
 
 
@@ -37,14 +36,12 @@ module.exports = function(rootNode, prerendered) {
 
 function attachRoute(rootNode, route) {
 	console.log('attachRoute()', route);
-	// var progressBar = globalRequire('progressBar');
 
 	page(route.path, function(context) {
 		if (window.wasPrerendered) {
 			window.wasPrerendered = false;
 			return;
 		}
-		// progressBar.addTask();
 
 
 
@@ -52,10 +49,10 @@ function attachRoute(rootNode, route) {
 			? $('.contentWrapper')
 			: $('#content').wrap('<div class="contentWrapper"></div>').closest('.contentWrapper');
 
+		$('#loading').stop().fadeIn(transitionTime/2);
+
 		// provide immediate user feedback via fadeout
-		// progressBar.addTask();
 		$oldContent.fadeOut(transitionTime, function(){
-			// progressBar.taskComplete();
 			$(this).remove()
 		});
 
@@ -66,32 +63,31 @@ function attachRoute(rootNode, route) {
 
 
 		var loadStart = Date.now();
-		// progressBar.addTask();
 		route.getView(templateRenderer, requestProps, function(err, results) {
-			// progressBar.taskComplete();
-			// console.log('results', results);
+			console.log('requestProps', requestProps);
+			console.log('results', results);
 
-			var $newContent = $(results.content).wrap('<div class="contentWrapper"><div id="content" class="container"></div></div>').closest('.contentWrapper').hide();
+			var $newContent = $($.parseHTML(results.contentHtml)).wrap('<div class="contentWrapper"><div id="content" class="container"></div></div>').closest('.contentWrapper').hide();
 			var loadComplete = Date.now();
 			var loadingTime = (loadComplete - loadStart);
 
 			// try to match the timing of the fadeout, but require least 50% of the transition time
 			var transitionRemaining = transitionTime - loadingTime;
 			var fadeInTime = Math.max(transitionRemaining, transitionTime / 2);
-			// console.log(fadeInTime, transitionRemaining, loadingTime)
+
+
+			$('#loading').stop().fadeOut(fadeInTime);
 
 			window.scrollTo(0, 0);
 			$newContent
 				.appendTo('body')
 				.fadeIn(fadeInTime, function(){
-					// progressBar.taskComplete();
-					// progressBar.done();
-					
+					$('title').text(results.meta.title);
+					$('meta[name="description"]').attr('content', results.meta.description);
+
 					$(window).trigger('hashchange');
 				});
 
-			$('title').text(results.meta.title);
-			$('meta[name="description"]').attr('content', results.meta.description);
 
 
 		});
