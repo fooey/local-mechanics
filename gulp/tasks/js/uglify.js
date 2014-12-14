44a781c428a5cc86db805c7a02ae1878c6ee6161
Jason Rushton <github.com@schtuph.com>
2014-12-13 18:24:28 -0700

    update packages

--- gulp/tasks/js/uglify.js
+++ gulp/tasks/js/uglify.js
@@ -6,17 +6,17 @@ var rename = require('gulp-rename');
 gulp.task('uglify', ['browserify'], function() {
 	var src = './public/dist/js/client.js';
 	var dest = './public/dist/js';
-	var srcMap = './public/dist/js/client.js.map';
+	// var srcMap = './public/dist/js/client.js.map';
 
-	console.log('uglify', src, dest, srcMap);
+	console.log('uglify', src, dest/*, srcMap*/);
 
 	var stream = gulp
 		.src(src)
 		.pipe(uglify('client.min.js', {
 			report: 'min',
 			stripBanners: true,
-			inSourceMap: srcMap,
-			outSourceMap: true,
+			// inSourceMap: srcMap,
+			// outSourceMap: true,
 			mangle: true,
 			compress: true,
 			output: {
@@ -28,7 +28,7 @@ gulp.task('uglify', ['browserify'], function() {
 		.pipe(gulp.dest(dest))
 		// .pipe(livereload()) //nodemon will trigger reload
 
-	
+
 	stream.on('error', function (err) {
 		console.log(err.toString());
 		this.emit("end");
